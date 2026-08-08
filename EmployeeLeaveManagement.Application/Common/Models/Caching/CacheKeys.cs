3b4b88aa2dacc389e544e03e9dce3c7b41b61903
Mina Lotfy <mina.lotfy@vetanoia.com>
2026-08-09 01:24:20 +0300

    added CacheService Interface and CacheKeys

---
+++ EmployeeLeaveManagement.Application/Common/Models/Caching/CacheKeys.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeLeaveManagement.Application.Common.Models.Caching
+{
+    public static class CacheKeys
+    {
+        public const string LeaveTypes = "leave-types";
+        public const string Holidays = "holidays";
+
+        public static string LeaveType(Guid id)
+            => $"leave-types:{id}";
+
+        public static string Holiday(Guid id)
+            => $"holidays:{id}";
+    }
+}
